b327dc1dc258967721e40dbcdb249ba67a67a433
Your Name <you@example.com>
2019-05-07 17:01:56 +0800

    update

--- aduit/fortify_run.py
+++ aduit/fortify_run.py
@@ -4,17 +4,12 @@ from xml.dom.minidom import parse
 import xml.dom.minidom
 import random
 import string
-import pymysql
 import codecs
 from .models import proj_info, vul_info
 from celery.decorators import task
-import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
 # from celery.task.schedules import crontab
 # from celery.decorators import periodic_task
 import requests
-import datetime
 from banruo.config import *
 
 
@@ -130,122 +125,3 @@ def push(gitaddress='', svnaddress='', name='', type=1, svnaccount='', svnpwd=''
     run(myfile, token)
 
 
-def send_email():
-    now_time = datetime.datetime.now()
-    now_time = str(now_time.strftime('%Y-%m-%d'))
-    sql = '''
-    SELECT bb.project as project_id,bb.name,COUNT(*) as total,IFNULL(d.new,0) ,GROUP_CONCAT(bb.id)as bug_id_list from
-    (SELECT a.id,c.name 
-     ,a.project from zt_bug a
-     LEFT JOIN zt_project c on a.project = c.id
-     WHERE a.status = 'active' and a.product = '3' and a.openedDate  > '%s' )bb
-    LEFT JOIN(select a.id,c.name 
-     ,a.project,count(1) as new from zt_bug a
-     LEFT JOIN zt_project c on a.project = c.id
-     WHERE a.status = 'active' and a.product = '3' and a.openedDate  > '%s' GROUP BY a.project) as d on d.name = bb.name  
-    group by bb.name
-        ''' % (now_time, now_time)
-    conn = pymysql.connect(host=MYSQL_HOST, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db=MYSQL_DATABASE, port=MYSQL_PORT,
-                           charset="utf8")
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    td = ''
-    for row in results:
-        contend = ''
-        try:
-            chandao_id = row[4].split(',')
-            for id in chandao_id:
-                contend = contend + u'''<a href='http://192.168.1.210:8888/zentao/bug-view-{0}.html'>{1}  </a>
-        '''.format(id, id)
-        except AttributeError:
-            pass
-        td = td + u'''   
-    <tr>
-              <td style="background:#e8eaeb,text-align:center"><a href="http://192.168.1.210:8888/zentao/project-bug-%s.html">%s</a></td>
-              <td style="text-align:center">%s</td>
-              <td style="text-align:center">%s</td>
-              <td>%s</td>
-            </tr>
-    ''' % (row[0], row[1], row[2], row[3], contend)
-    sql = '''
-    SELECT bb.product,bb.name,bb.total,IFNULL(d.new,0),GROUP_CONCAT(d.id)as bug_id_list 
-    from  (select a.product,c.name,count(*) as total from zt_bug a
-             LEFT JOIN zt_product c on a.product = c.id
-            WHERE a.status = 'active' and a.openedBy = '1' and a.product != '3' 
-            group by a.product,c.name)bb
-    left join (SELECT a.id,a.product,c.name,count(1) as new from zt_bug a
-           LEFT JOIN zt_product c on a.product = c.id
-          WHERE a.status = 'active' and a.openedBy = '1' and a.product != '3' and a.openedDate  > '%s'
-           group by   a.product,c.name
-          ) as d  on d.`name` = bb.`name` and d.product = bb.product
-    group by bb.name
-        ''' % now_time
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    testtd = ''
-    for row in results:
-        contend = ''
-        try:
-            chandao_id = row[4].split(',')
-            for id in chandao_id:
-                contend = contend + u'''<a href='http://test.com/index.php?m=bug&f=view&bugID={0}'>{1}  </a>
-        '''.format(id, id)
-        except AttributeError:
-            pass
-        testtd = testtd + u'''   
-    <tr>
-              <td style="background:#e8eaeb,text-align:center"><a href="http://test.com/index.php?m=bug&f=browse&productID=%s">%s</a></td>
-              <td style="text-align:center">%s</td>
-              <td style="text-align:center">%s</td>
-              <td>%s</td>
-            </tr>
-    ''' % (row[0], row[1], row[2], row[3], contend)
-    html = u'''
-    <html>
-    <head>
-    </head>
-    <body>
-    <table border="1">
-    <h2>代码审计项目</h2>
-    <tr>
-    <th>git项目</th>
-    <th>总数</th>
-    <th>新增数</th>
-    <th>禅道ID</th>
-    </tr>
-    ''' + td + u'''
-    </table>
-    </br>
-    <h2>渗透测试项目</h2>
-    <table border="1">
-    <tr>
-    <th>测试项目</th>
-    <th>总数</th>
-    <th>新增数</th>
-    <th>禅道ID</th>
-    </tr>
-    ''' + testtd + u'''
-    </table>
-    </br>
-    <b>注意：代码审计的结果是基于github项目进行审计，每个项目的负责人我已经在禅道上面指派了。</b></br>
-    <b>还有就是漏洞误报的问题，审计的结果是正确的，但是无法进行攻击。例如：发现了一个SQL注入的漏洞，但是由于这个查询仅供内部查询调用，即黑客无法在外部找到这个漏洞</b></br>
-    <b>如果发现是误报，可以直接选择关闭，当然，你也可以按照我的修复建议进行修复，加强代码质量。</b></br>
-    <b>如果你已经修复的话，请告诉你在那个分支修复了，以便我重新检测。（代码审计系统默认拉取master分支）</b></br>
-    <b>谢谢各位的合作！！！</b>
-    </body>
-    </html>
-    '''
-    mail_host = MAIL_HOST
-    mail_user = MAIL_USER
-    mail_pass = MAIL_PASSWORD
-    subject = '每天禅道安全审计结果'
-    sender = mail_user
-    receivers = [mail_user]  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
-    message = MIMEText(html, 'html', 'utf-8')
-    message['From'] = Header("安全审计", 'utf-8')
-    message['Subject'] = Header(subject, 'utf-8')
-    smtpObj = smtplib.SMTP()
-    smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
-    smtpObj.login(mail_user, mail_pass)
-    smtpObj.sendmail(sender, receivers, message.as_string())
