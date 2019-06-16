9a9f943cbf45ad8c9f8b1b86aaf632b1d45f3ea8
Your Name <you@example.com>
2019-06-16 15:25:40 +0800

    zzz

--- banruo/settings.py
+++ banruo/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': 'banruo',
         'HOST':'127.0.0.1',
         'PORT':'3306',
-        'USER':'django',
+        'USER':'root',
         'PASSWORD':'123456',
         'OPTIONS': {
                     'charset': 'utf8mb4',
